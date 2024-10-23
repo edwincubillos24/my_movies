a737e8fab3522eece64d8c3f83cfe49a7d5c7c02
edwinacubillos <edwinacubillos@gmail.com>
2024-10-23 16:30:25 -0500

    23-oct: Implementación de WebServices con http consumiendo la API de TheMovieDB

--- lib/pages/movie_db_page.dart
+++ lib/pages/movie_db_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_movies/repository/moviedb_api.dart';
+
+import '../models/apimoviedb_top_rated_response.dart';
 
 class MovieDbPage extends StatefulWidget {
   const MovieDbPage({super.key});
@@ -8,11 +11,42 @@ class MovieDbPage extends StatefulWidget {
 }
 
 class _MovieDbPageState extends State<MovieDbPage> {
+  final MoviedbApi moviedbApi = MoviedbApi();
+  List<Results> listMovies = <Results>[];
+
+  Future<void> _getTopRated() async {
+    var results = await moviedbApi.getTopRated();
+    setState(() {
+      listMovies = results.results!;
+    });
+  }
+
+  @override
+  void initState() {
+    _getTopRated();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("MovieDB"),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: ListView.builder(
+              itemCount: listMovies.length,
+              itemBuilder: (BuildContext context, int index) {
+                Results movie = listMovies[index];
+                return Card(
+                  child: ListTile(
+                    title: Text(movie.title!),
+                    subtitle: Text("Average: ${movie.voteAverage}"),
+                    leading: Image.network(
+                        "https://image.tmdb.org/t/p/w500/${movie.backdropPath}"),
+                  ),
+                );
+              }),
+        ),
       ),
     );
   }
---
+++ lib/repository/moviedb_api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:my_movies/models/apimoviedb_top_rated_response.dart';
+import 'package:http/http.dart' as http;
+
+class MoviedbApi {
+
+  Future<ApimoviedbTopRatedResponse> getTopRated() async {
+    final response =
+        await http.get(Uri.parse('https://api.themoviedb.org/3/movie/top_rated?api_key=ff29f617b45b36aab5aa78a6fa04677f'));
+        print('Respuesta: ${response.body}');
+        if (response.statusCode == 200){
+          return ApimoviedbTopRatedResponse.fromJson(jsonDecode(response.body));
+        } else {
+          throw Exception('Falla al cargar las peliculas');
+        }
+  }
+
+}
